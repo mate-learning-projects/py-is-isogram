608b7d8a225d95f5766f4a4b51d9c682d08deea8
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-25 11:00:12 +0200

    Add Is isogram solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+is_isogram("playgrounds") is True
+is_isogram("look") is False
+is_isogram("Adam") is False
+is_isogram("") is True
+
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ],
+)
+def test_is_isogram(word, expected):
+    assert is_isogram(word) == expected
